3eedce5ffcd56c1ad8605db9f121bab6765598ff
OscarDeivid <37225754+OscarDeivid@users.noreply.github.com>
2018-05-17 16:24:33 -0500

    Update tabla.php
    El botón que programamos para borrar cada registro, debe tener un ícono (imagen) pequeño con una equis.
    Linea 5 <img src="XX.jpg" width="60" height="40"

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,7 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('id','nombre','fecha', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="XX.jpg" width="60" height="40" </button>');
+ $cols = array('id','nombre','fecha', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="XX.jpg" width="60" height="40"</button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
